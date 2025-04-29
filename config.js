306d92eeda49b3afb84920de28479c3668e1b06e
Dilshan841 <rathnayakehemantha427@gmail.com>
2025-04-29 11:17:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ module.exports = {
   BOT_NAME: process.env.BOT_NAME || 'DILSHAN-MD',
   OWNER_NAME: process.env.OWNER_NAME || 'Dilshan Ashinsa',
   OWNER_NUMBER: process.env.OWNER_NUMBER || '94772194789'
-};
+  ALIVE_IMG: process.env.ALIVE_IMG || ''
+  ALIVE_MSG: process.env.ALIVE_MSG || '👋Hello I am DILSHAN MD I am Alive now
